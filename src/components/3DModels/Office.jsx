c291c49bbed5dd27ccc11283600c1b28fdb3e850
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2025-09-27 16:25:09 +0200

    Update src/components/3DModels/Office.jsx

--- src/components/3DModels/Office.jsx
+++ src/components/3DModels/Office.jsx
@@ -11,7 +11,7 @@ import React from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export default function Model(props) {
-  const { nodes, materials } = useGLTF('/office/office.gltf')
+  const { nodes, materials } = useGLTF(process.env.PUBLIC_URL + '/office/office.gltf')
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
@@ -23,4 +23,4 @@ export default function Model(props) {
   )
 }
 
-useGLTF.preload('/office/office.gltf')
+useGLTF.preload(process.env.PUBLIC_URL + '/office/office.gltf')
